5d1791e7d51ed51ae4bedbf99e870fd629023064
javigarciaar <javierdgarcia475@gmail.com>
2026-02-06 17:53:33 -0400

    Modificacion del menu

--- src/main/java/com/comedor/Main.java
+++ src/main/java/com/comedor/Main.java
@@ -2,19 +2,15 @@ package com.comedor;
 
 import javax.swing.SwingUtilities;
 
-import com.comedor.control.LoginController;
-import com.comedor.view.LoginView;
+import com.comedor.control.AppCoordinator;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         SwingUtilities.invokeLater(() -> {
-            
-            LoginView loginView = new LoginView();
 
-            new LoginController(loginView);
-
-            loginView.setVisible(true);
+            AppCoordinator app = new AppCoordinator();
+            app.start();
         });
 
     }
